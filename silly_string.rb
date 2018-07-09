fc0f6939f1164afcd2db462c1fcffcbec4e5a4dd
Kate1234567891 <katehalle617@icloud.com>
2018-07-09 19:10:58 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,9 +1,9 @@
 
-# Challenge 1: Fill this string in with your favorite word
+# Challenge 1: Kate
 
-your_favorite_word = ""
+your_favorite_word = "Kate"
 
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is #Kate"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
